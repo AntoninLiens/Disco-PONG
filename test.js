41552f858f43c052552bfe17220e3f6e2ee85c0c
Antonin Liens <aliens@fu-r9-p8.s19.be>
2023-01-23 18:42:21 +0100

    le push

---
+++ test.js
@@ -0,0 +1,16 @@
+let signup = document.querySelector(".signup");
+let login = document.querySelector(".signin");
+let slider = document.querySelector(".slider");
+let formSection = document.querySelector(".formBox");
+let bodysection = document.querySelector("body");
+let color = bodysection.querySelector(".box");
+
+signup.addEventListener("click", () => {
+	slider.classList.add("moveSlider");
+	formSection.classList.add("moveFormBox");
+});
+
+login.addEventListener("click", () => {
+	slider.classList.remove("moveSlider");
+	formSection.classList.remove("moveFormBox");
+});
