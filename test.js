d485b12f89a6253d53248d46d1202ca166ae64e0
Antonin Liens <aliens@fu-r7-p8.s19.be>
2023-01-27 19:41:37 +0100

    play button add && friends onglet ok && menu ok

--- test.js
+++ test.js
@@ -9,6 +9,12 @@ let addBox = document.querySelector(".addBox");
 let body = document.querySelector("body");
 let title = document.querySelector("title");
 
+let menu = document.querySelector(".menu");
+let chat = document.querySelector(".chat");
+
+let menuBtn = document.getElementById("menuBtn");
+let chatBtn = document.getElementById("chatBtn");
+
 signup.addEventListener("click", () => {
 	slider.classList.add("moveSlider");
 	formSection.classList.add("moveFormBox");
@@ -28,3 +34,25 @@ function setHomePage() {
 
 loginBtn.addEventListener("click", () => { setHomePage(); });
 registerBtn.addEventListener("click", () => { setHomePage(); });
+
+menuBtn.addEventListener("click", () => {
+	if (menuBtn.innerHTML === "menu") {
+		menu.style.left = 0;
+		menuBtn.innerHTML = "close";
+	}
+	else {
+		menuBtn.innerHTML = "menu";
+		menu.style.left = "-60px";
+	}
+});
+
+chatBtn.addEventListener("click", () => {
+	if (chatBtn.innerHTML === "chat") {
+		chat.style.right = 0;
+		chatBtn.innerHTML = "close";
+	}
+	else {
+		chatBtn.innerHTML = "chat";
+		chat.style.right = "-100%";
+	}
+});
