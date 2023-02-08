d426f763fbd5164890aeceae8a1ca7c1847007d3
Zaine-el-abidine Minhas <zminhas@fu-r8-p8.s19.be>
2023-02-08 18:05:51 +0100

    premier vrai prototype de profile

--- app/src/components/Profile/Profile.tsx
+++ app/src/components/Profile/Profile.tsx
@@ -5,13 +5,16 @@ export default function Profile() {
 		<div className="profile">
 			<div className="profile_box">
 				<div className="profile_pfp"></div>
-				<div className="profile_name">name !</div>
-				<div className="profile_wins">Wins: </div>
-				<div className="profile_lvl">Level: </div>
+				<div className="profile_name">ToTo</div>
+				<div className="profile_wins">Wins: 11</div>
+				<div className="profile_loose">Looses: 22</div>
+				<div className="profile_score">Score: 1000</div>
+				<div className="profile_lvl">Level: 1</div>
 				<div className="profile_xpBar">
 					<div className="profile_xp"></div>
 				</div>
 			</div>
+			<div className="profile_return">return</div>
 		</div>
 	);
 }
