3135ed1a07772e525f03bc3ddc2533f45f9f8103
Antonin Liens <aliens@fu-r9-p7.s19.be>
2023-02-08 18:10:52 +0100

    debut affNavBar

--- app/src/components/NavBar/NavBar.tsx
+++ app/src/components/NavBar/NavBar.tsx
@@ -3,13 +3,19 @@ import { FaUser, FaShoppingCart } from "react-icons/fa"
 import { MdLeaderboard, MdSettingsSuggest } from "react-icons/md"
 import { ImStatsBars } from "react-icons/im"
 import { BsClockHistory, BsPeopleFill } from "react-icons/bs"
+import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai"
 import "./NavBar.css";
 
 export default function NavBar({id}: any) {
     // States
 
+    function affNavBar() {
+
+    }
+
     return (
         <div className="NavBar">
+            <button onClick={affNavBar}><AiOutlineMenuFold /></button>
             <ul>
                 <li><Link to={`/homePage/${id}/profile`}><FaUser size={24}/></Link></li>
                 <li><Link to="/shop"><FaShoppingCart size={24}/></Link></li>
