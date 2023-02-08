1a78f28011b0f2b1422aa16d90619357ddefbf21
Zaine-el-abidine Minhas <zminhas@fu-r8-p8.s19.be>
2023-02-08 16:40:10 +0100

    on fait la profile page

--- app/src/components/Profile/Profile.tsx
+++ app/src/components/Profile/Profile.tsx
@@ -1,5 +1,17 @@
+import "./Profile.css"
+
 export default function Profile() {
-    return (
-        <div>Profile</div>
-    )
+	return (
+		<div className="profile">
+			<div className="profile_box">
+				<div className="profile_pfp"></div>
+				<div className="profile_name">name !</div>
+				<div className="profile_wins">Wins: </div>
+				<div className="profile_lvl">Level: </div>
+				<div className="profile_xpBar">
+					<div className="profile_xp"></div>
+				</div>
+			</div>
+		</div>
+	);
 }
