1f8e173832ba826f489c66bd72198971a1e629bc
Zaine-el-abidine Minhas <zminhas@mi-r2-p7.s19.be>
2023-02-10 17:50:10 +0100

    premiere history page

--- app/src/components/History/History.tsx
+++ app/src/components/History/History.tsx
@@ -1,5 +1,31 @@
+import { useParams } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
+import "./History.css"
+
 export default function History() {
-    return (
-        <div>History</div>
-    )
+
+	let { id } = useParams();
+
+	if (!id)
+		id = "error";
+
+	return (
+		<div className="historyPage">
+			<NavBar id={id} />
+			<div className="history_box">
+				<h1>Your History:</h1>
+				<div className="history_match">
+					<div className="history_playerOne">
+						<div className="history_playerOnePfp"></div>
+						<div className="history_playerOneName">{id}</div>
+					</div>
+					<div className="history_vs">VS</div>
+					<div className="history_playerTwo">
+						<div className="history_playerTwoName">JeeJ</div>
+						<div className="history_playerTwoPfp"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- app/src/components/NavBar/NavBar.tsx
+++ app/src/components/NavBar/NavBar.tsx
@@ -1,45 +1,45 @@
 import { Link } from "react-router-dom";
 import { FaUser, FaShoppingCart } from "react-icons/fa"
-import { MdLeaderboard, MdSettingsSuggest } from "react-icons/md"
+import { MdLeaderboard, MdSettingsSuggest, MdMenu } from "react-icons/md"
 import { ImStatsBars } from "react-icons/im"
 import { BsClockHistory, BsPeopleFill } from "react-icons/bs"
-import { AiOutlineMenuFold, AiOutlineMenuUnfold } from "react-icons/ai"
+import { GrClose } from "react-icons/gr"
 import "./NavBar.css";
 import { useState } from "react";
 
 export default function NavBar({id}: any) {
-    // States
-    const [aff, setAff] = useState("unfold");
-    const [fold, setFold] = useState("hide");
-    const [unfold, setUnfold] = useState("");
+	// States
+	const [aff, setAff] = useState("unfold");
+	const [fold, setFold] = useState("hide");
+	const [unfold, setUnfold] = useState("");
 
 
-    function affNavBar() {
-        if (aff === "unfold") {
-            setAff("fold");
-            setUnfold("hide");
-            setFold("");
-        }
-        else {
-            setAff("unfold");
-            setFold("hide");
-            setUnfold("");
-        }
-    }
+	function affNavBar() {
+		if (aff === "unfold") {
+			setAff("fold");
+			setUnfold("hide");
+			setFold("");
+		}
+		else {
+			setAff("unfold");
+			setFold("hide");
+			setUnfold("");
+		}
+	}
 
-    return (
-        <div className={`NavBar ${aff}`}>
-            <a className={`affNavBar ${fold}`} onClick={affNavBar}><AiOutlineMenuFold size={24} /></a>
-            <a className={`affNavBar ${unfold}`} onClick={affNavBar}><AiOutlineMenuUnfold size={24}/></a>
-            <ul>
-                <li><Link to={`/homePage/${id}/profile`}><FaUser size={24}/></Link></li>
-                <li><Link to="/shop"><FaShoppingCart size={24}/></Link></li>
-                <li><Link to={`/homePage/${id}/leaderbords`}><MdLeaderboard size={24}/></Link></li>
-                <li><Link to={`/homePage/${id}/stats`}><ImStatsBars size={24}/></Link></li>
-                <li><Link to={`/homePage/${id}/history`}><BsClockHistory size={24}/></Link></li>
-                <li><Link to={`/homePage/${id}/social`}><BsPeopleFill size={24}/></Link></li>
-                <li id="settings"><Link to={`/homePage/${id}/settings`}><MdSettingsSuggest size={24}/></Link></li>
-            </ul>
-        </div>
-    );
+	return (
+		<div className={`NavBar ${aff}`}>
+			<a className={`affNavBar ${fold}`} onClick={affNavBar}><GrClose size={24} /></a>
+			<a className={`affNavBar ${unfold}`} onClick={affNavBar}><MdMenu size={24}/></a>
+			<ul>
+				<li><Link to={`/homePage/${id}/profile`}><FaUser size={24}/></Link></li>
+				<li><Link to="/shop"><FaShoppingCart size={24}/></Link></li>
+				<li><Link to={`/homePage/${id}/leaderbords`}><MdLeaderboard size={24}/></Link></li>
+				<li><Link to={`/homePage/${id}/stats`}><ImStatsBars size={24}/></Link></li>
+				<li><Link to={`/homePage/${id}/history`}><BsClockHistory size={24}/></Link></li>
+				<li><Link to={`/homePage/${id}/social`}><BsPeopleFill size={24}/></Link></li>
+				<li id="settings"><Link to={`/homePage/${id}/settings`}><MdSettingsSuggest size={24}/></Link></li>
+			</ul>
+		</div>
+	);
 }
