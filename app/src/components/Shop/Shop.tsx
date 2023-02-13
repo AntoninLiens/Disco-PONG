6275e705d7a44c5608371188b39e9e3952ef5f7d
Zaine-el-abidine Minhas <zminhas@mi-r4-p1.s19.be>
2023-02-13 17:43:08 +0100

    colored shop

--- app/src/components/Shop/Shop.tsx
+++ app/src/components/Shop/Shop.tsx
@@ -2,75 +2,75 @@ import NavBar from "../NavBar/NavBar"
 import "./Shop.css"
 
 export default function Shop() {
-    return (
-        <div className="ShopPage">
-            <div className="shopTitle">
-                <h2>Disco-PONG</h2>
-                <h1>SHOP</h1>
-            </div>
-            <NavBar />
-            <ul className="shopList">
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-                <li className="shopItem">
-                    <a href="#"></a>
-                    <span>PRICE</span>
-                </li>
-            </ul>
-        </div>
-    )
+	return (
+		<div className="ShopPage">
+			<div className="shopTitle">
+				<h2>Disco-PONG</h2>
+				<h1>SHOP</h1>
+			</div>
+			<NavBar />
+			<ul className="shopList">
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+				<li className="shopItem">
+					<a href="#"></a>
+					<span>PRICE</span>
+				</li>
+			</ul>
+		</div>
+	)
 }
