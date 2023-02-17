dc80c4a895d60c367e512644f0397a7e0a933f3b
CassTirio22 <cassandre.tirions@yahoo.be>
2023-02-17 15:08:53 +0100

    push express

--- app/src/components/Settings/Settings.tsx
+++ app/src/components/Settings/Settings.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import NavBar from "../NavBar/NavBar";
@@ -12,9 +12,13 @@ export default function Settings() {
 
 	const [ controlToggle, setControlToggle ] = useState("isHide");
 	const [ HUDToggle, setHUDToggle ] = useState("isHide");
-	const [firstKeyPressed, setFirstKeyPressed] = useState<string>(intToChar(97));
+	const [ PSWToggle, setPSWToggle ] = useState("isHide");
 
-	const handleClick = (type: string) => {
+	const [upKeyValue, setUpKeyValue] = useState<string>(intToChar(87));
+	const [downKeyValue, setDownKeyValue] = useState<string>(intToChar(83));
+	const [passwordVal, setPasswordVal] = useState<string>("");
+
+	const showHideSettingItems = (type: string) => {
 		if (type === "controls") {
 			if (controlToggle === "isHide")
 				setControlToggle("isShow");
@@ -27,18 +31,41 @@ export default function Settings() {
 			else
 				setHUDToggle("isHide");
 		}
+		else if (type === "PSW") {
+			if (PSWToggle === "isHide")
+				setPSWToggle("isShow");
+			else
+				setPSWToggle("isHide");
+		}
 	}
 
 	function intToChar(nbr: number) {
 		return String.fromCharCode(nbr);
 	  }
 
-	const handleButtonClick = () => {
-		document.addEventListener("keydown", handleKeyPress);
+	const handleButtonClick = (type: string) => {
+		if (type === "moveUp")
+			document.addEventListener("keydown", handleMoveUpKeyPress);
+		else if (type === "moveDown")
+			document.addEventListener("keydown", handleMoveDownKeyPress);
 	};
-	const handleKeyPress = (event: KeyboardEvent) => {
-		setFirstKeyPressed(intToChar(event.keyCode));
-		document.removeEventListener("keydown", handleKeyPress)
+	const handleMoveUpKeyPress = (event: KeyboardEvent) => {
+		setUpKeyValue(intToChar(event.keyCode));
+		document.removeEventListener("keydown", handleMoveUpKeyPress)
+	}
+
+	const handleMoveDownKeyPress = (event: KeyboardEvent) => {
+		setDownKeyValue(intToChar(event.keyCode));
+		document.removeEventListener("keydown", handleMoveDownKeyPress)
+	}
+
+	const handleChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+		if (event.target.value.length > 12)
+			alert("Password must be under 12 chars")
+		else if (event.target.value.length < 6)
+			alert("Password must be upper 6 chars")
+		else
+			setPasswordVal(event.target.value)
 	}
 
 	return (
@@ -49,31 +76,38 @@ export default function Settings() {
 				<h1>Settings</h1>
 			</div>
 			<div className="mySettings">
+
 				<div className="controls settingsItem">
-					<button onClick={() => handleClick("controls")} className="controlsButton settingsButton">Controls</button>
+					<button onClick={() => showHideSettingItems("controls")} className="controlsButton settingsButton">Controls</button>
 					<div className={`mainSettingsItems ${controlToggle}`}>
 						<div className="moveUp settingsPair">
 							<div className="settingsKey">Move up</div>
-							<div className="settingsVal">w</div>
+							<button onClick={() => handleButtonClick("moveUp")} className="settingsVal">{upKeyValue}</button>
 						</div>
 						<div className="moveDown settingsPair">
 							<div className="settingsKey">Move down</div>
-							<button onClick={handleButtonClick} className="settingsVal">{firstKeyPressed}</button>
+							<button onClick={() => handleButtonClick("moveDown")} className="settingsVal">{downKeyValue}</button>
 						</div>
 					</div>
 				</div>
 
-
 				<div className="HUD settingsItem">
-					<button onClick={() => handleClick("HUD")} className="HUDButton settingsButton">HUD</button>
+					<button onClick={() => showHideSettingItems("HUD")} className="HUDButton settingsButton">HUD</button>
 					<div className={`mainSettingsItems ${HUDToggle}`}>
 						<div className="moveUp">up = w</div>
 						<div className="moveDown">down = s</div>
 					</div>
 				</div>
 
-
-				<div className="password isHide settingsItem">Password</div>
+				<div className="PSW settingsItem">
+					<button onClick={() => showHideSettingItems("PSW")} className="PSWButton settingsButton">Password</button>
+					<div className={`mainSettingsItems ${PSWToggle}`}>
+						<div className="password settingsPair">
+							<div className="settingsKey">Password</div>
+							<input onChange={handleChangePassword} type="password" className="settingsVal" value={passwordVal}/>
+						</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
